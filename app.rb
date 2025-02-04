e77b1e1b3c1e75434d3cc4d2025404b960eaf8a3
elinSoderstrom <151535511+elinSoderstrom@users.noreply.github.com>
2025-02-04 16:21:20 +0100

    v. 6

--- app.rb
+++ app.rb
@@ -3,3 +3,24 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'becrypt'
+
+get("/explore") do
+
+    slim(:explore)
+    db=
+
+end 
+
+get("/create") do
+
+    slim(:create)
+    db=
+
+end
+
+get("/profile") do
+
+    slim(:profile)
+    db=
+
+end
