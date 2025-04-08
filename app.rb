830086c700ecb52139fe3ed440027068d71d087f
elinSoderstrom <151535511+elinSoderstrom@users.noreply.github.com>
2025-04-08 18:10:19 +0200

    några veckors arbete

--- app.rb
+++ app.rb
@@ -13,11 +13,22 @@ get("/explore") do
 
 end 
 
+
 get("/create") do
 
     slim(:create)
     
+end
 
+post("/create") do
+
+    name = params[:name]
+    ingredients =params[:ingredients]
+    instructions = params[:instructions]
+    db = SQLite3::Database.new("db/db.db")
+    db.execute("INSERT INTO recepies (name, ingredients, instructions) VALUES (?,?,?)", [name, ingredients, instructions])
+    redirect('/create')
+    
 end
 
 get("/profile") do
@@ -25,4 +36,62 @@ get("/profile") do
     slim(:profile)
     
 
+end
+
+get("/delete") do
+
+    slim(:delete)
+    
+end
+
+post("/delete") do
+
+    id = params[:name]
+    db = SQLite3::Database.new("db/db.db")
+    db.execute("DELETE FROM recepies WHERE name = ?", id)
+    redirect('/create')
+end
+
+post("/user") do
+    user= params["user"]
+    pwd= params["pwd"]
+    pwd_confirm= params["pwd_confirm"]
+    db = SQLite3::Database.new("db/db.db")
+    db.execute("SELECT id FROM users WHERE
+    user=?",user)
+    if result.empty?
+        if pwd==pwd_confirm
+            pwd_digest=BCrypt::Password
+            .create(pwd)
+            db.execute("INSERT INTO users(user,
+            pwd_digest) VALUES(?,?)",user,pwd_digest)
+            redirect("/explore")
+        else
+            redirect("/error") #Lösenord matchar ej
+        end
+    
+    else
+        redirect("/login") #User existerar redan
+        end
+        
+    end
+end
+
+post("/login") do
+    user = params["user"]
+    pwd = params["pwd"]
+    db = SQLite3::Database.new("db/db.db")
+    result=db.execute("SELECT id,pwd_digest FROM
+    users WHERE user=?",user)
+    if result.empty?
+        redirect("/error") #Fel lösenord/användarnamn
+    end
+    user_id = result.first["id"]
+    pwd_digest = result.first["pwd_digest"]
+    if BCrypt::Password.new(pwd_digest) == pwd
+        session[:user_id] = user_id
+        redirect("/welcome")
+    else
+        redirect("/error") #Fel lösenord/username
+    end
 end
